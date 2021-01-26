4a8c96a03e11f59050e9e624bd538ac84ddc2dfa
Will Taylor <willtaylor.developer@gmail.com>
2021-01-26 17:35:54 +0000

    Tailwind setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-	export let name;
+	import MineSweeper from './components/MineSweeper.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<MineSweeper/>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style global lang="postcss">
+  /* only apply purgecss on utilities, per Tailwind docs */
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @tailwind utilities;
 </style>
---
+++ src/components/MineSweeper.svelte
@@ -0,0 +1,3 @@
+<main>
+  <h1 class="bg-blue-700">Hello world</h1>
+</main>
